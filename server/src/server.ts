7de68c3916ae0f9d4c809d9c31fb0443d68c6979
jahun <99630193+MrNubi@users.noreply.github.com>
2023-01-08 08:06:13 +0900

    12345678

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import { AppDataSource } from './data-source';
 
 const app = express();
 
@@ -11,4 +12,12 @@ app.get('/', (_, res) => res.send('Running!'));
 let port = 4000;
 app.listen(port, async () => {
   console.log(`running at http://localhost:${port}`);
+
+  // once in your application bootstrap
+  AppDataSource.initialize()
+    .then(() => {
+      // here you can start to work with your database
+      console.log('database initialized');
+    })
+    .catch((error) => console.log(error));
 });
