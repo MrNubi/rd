e9ff8fcf3cf0c41cddfeabe6f8dcf9a525e563f3
jahun <99630193+MrNubi@users.noreply.github.com>
2023-01-31 23:02:52 +0900

    01311102 fontawsome 적용 및 댓글 구현(대댓글 less)

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -6,8 +6,13 @@ import { useRouter } from 'next/router';
 import NavBar from '../components/Navbar';
 import { SWRConfig } from 'swr';
 import axios from 'axios';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import head from 'next/head';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
+  config.autoAddCss = false;
   const { pathname } = useRouter();
   const authRoutes = ['/register', '/login'];
   const authRoute = authRoutes.includes(pathname);
@@ -23,13 +28,23 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   };
   return (
-    <SWRConfig value={{ fetcher }}>
-      <AuthProvider>
-        {!authRoute && <NavBar />}
-        <div className={authRoute ? '' : 'pt-12'}>
-          <Component {...pageProps} />
-        </div>
-      </AuthProvider>
-    </SWRConfig>
+    <>
+      <Head>
+        <script
+          defer
+          src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"
+          integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <SWRConfig value={{ fetcher }}>
+        <AuthProvider>
+          {!authRoute && <NavBar />}
+          <div className={authRoute ? '' : 'pt-12'}>
+            <Component {...pageProps} />
+          </div>
+        </AuthProvider>
+      </SWRConfig>
+    </>
   );
 }
